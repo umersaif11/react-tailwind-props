3993440ba72748c2bca44d45a5e8646a46e98fc5
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-01-30 14:31:35 +0500

    Use of props in parent and then reading the props object by destructuring in the child and making ultimately reuseable components

--- react-tailwind-props/src/App.jsx
+++ react-tailwind-props/src/App.jsx
@@ -2,6 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,6 +10,22 @@ function App() {
   return (
     <>
       <h1 className='bg-green-500 text-black p-4 rounded-3xl mb-4'>Tailwind Testing</h1>  
+      <Card 
+      username = "Umar, The builder"
+      someObj = {{
+        channel: "Chai aur Code",
+        instructor: "Hitesh Ch"
+      }}
+      age = {20}
+      />
+      <Card
+      username = "Hitesh, The builder"
+      someObj = {{
+        channel: "Coffe aur Code",
+        instructor: "Unkown"
+      }}
+      age = {21}
+      />
     </>
   )
 }
